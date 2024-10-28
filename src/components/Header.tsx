4bb3d85427c5222a37463bf642728a2d4e1a1d8a
poulami-sau <109125687+poulami-sau@users.noreply.github.com>
2024-10-28 19:43:21 -0400

    added header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return(
+        <div className="header">
+            <div className="logo">
+                <img src="src/assets/Logo White.svg"/>
+            </div>
+        </div>
+    );
+}
