087d0f043d2794b7620ae52be9caf557d338f580
Brian Travis <bztravis88@gmail.com>
2024-12-09 02:08:22 -0500

    Fix loading scale origin

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,8 +2,8 @@ import Logo from '../assets/LogoBlack.svg';
 
 export function Loading() {
   return (
-    <div className='h-full flex flex-col justify-center items-center gap-8 opacity-50'>
-      <div className='[transform:scale(2)]'>
+    <div className='[height:calc(100vh-72px)] flex flex-col justify-center items-center gap-8 animate-pulse'>
+      <div className='[transform:scale(2)] [transform-origin:center_center]'>
         <Logo />
       </div>
     </div>
